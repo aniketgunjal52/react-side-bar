b5a43ab12fffef1f1a8c20de77bee652e1a46ef4
aniketgunjal52 <aniketgunjal52@gmail.com>
2025-03-25 00:27:35 +0530

    routes done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route }  from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Analytics from './pages/Analytics';
+import FileManager from './pages/FileManager';
+import Messages from './pages/Messages';
+import Order from './pages/Order';
+import Saved from './pages/Saved';
+import Setting from './pages/Setting';
+import Users from './pages/Users';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/filemanager" element={<FileManager />} />
+          <Route path="/messages" element={<Messages />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/users" element={<Users />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
